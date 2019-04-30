db2bb6758efa26da2bf4d196db1f12a4dfaed48c
Brian Todd <20519009+brian-todd@users.noreply.github.com>
2019-04-30 16:05:49 -0400

    Refactor requester code

--- network/requester.py
+++ network/requester.py
@@ -21,18 +21,34 @@ class FXTickDataRequester(object):
         :params request_date: Datetime object containing all relevant date parts.
         '''
 
-        self.request_date = request_date
-
-        # Parameters necessary for constructing URL.
-        # Convert to string and verify that each parameter has the correct format.
+        self.request_date: datetime = request_date
         self.currency: str = currency
-        self.year: str = str(request_date.year)
-        self.month: str = "{0:0>2}".format(request_date.month - 1)
-        self.day: str = "{0:0>2}".format(request_date.day)
-        self.hour: str = "{0:0>2}".format(request_date.hour)
+
+        # Validate and parse datetime information.
+        self.year, self.month, self.day, self.hour = self._parse_input_date(self.request_date)
 
         # Parametrized URL for requests.
-        self.DUKAS_BASE_URL = f'http://www.dukascopy.com/datafeed/{self.currency}/{self.year}/{self.month}/{self.day}/{self.hour}h_ticks.bi5'
+        self.DUKAS_BASE_URL: str = f'http://www.dukascopy.com/datafeed/{self.currency}/{self.year}/{self.month}/{self.day}/{self.hour}h_ticks.bi5'
+
+    def _parse_input_date(self, request_date: datetime) -> tuple:
+        '''
+        Parse input date and return a tuple outlining all necessary params.
+
+        :params request_date: Datetime of desired request datetime.
+        :returns date_parts: Tuple containing individual date parts.
+        '''
+
+        assert isinstance(request_date, datetime), f'Input argument is not datetime: {type(request_date)}'
+        try:
+            year: str = str(request_date.year)
+            month: str = "{0:0>2}".format(request_date.month - 1)
+            day: str = "{0:0>2}".format(request_date.day)
+            hour: str = "{0:0>2}".format(request_date.hour)
+
+        except Exception as e:
+            raise Exception(f'Could not parse input datetime: {request_date}')
+
+        return year, month, day, hour
 
     def request(self) -> bytes:
         '''
