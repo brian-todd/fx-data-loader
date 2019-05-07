982d280616bd0dfa39cbdf87a83d421dbfb95277
Brian Todd <20519009+brian-todd@users.noreply.github.com>
2019-05-06 22:49:01 -0400

    Modify ticks processor and update test suites

--- tests/test_processors/test_tabular_processor.py
+++ tests/test_processors/test_tabular_processor.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 
 import numpy as np
@@ -24,6 +25,14 @@ class TestTickDataTabularProcessor(TestTickDataBaseUtils, unittest.TestCase):
         Validate that the processor writes tab delimited data.
         '''
 
+        # Delete the test output path if present. Otherwise, make said path.
+        if os.path.exists(self.data_output_path):
+            shutil.rmtree(self.data_output_path)
+            os.mkdir(self.data_output_path)
+
+        else:
+            os.mkdir(self.data_output_path)
+
         # Iterate through five days worth of request dates.
         for request_date in [self.start_date + timedelta(i) for i in range(5)]:
             requester = FXTickDataRequester(self.currency, request_date)
@@ -37,24 +46,31 @@ class TestTickDataTabularProcessor(TestTickDataBaseUtils, unittest.TestCase):
             # Write sample data using custom writer.
             # If there are any errors writing the data, then fail the test.
             try:
-                sample = ticks.head(25).copy()
-                processor.write(sample, self.data_output_path)
+                processor.write(ticks, self.data_output_path)
 
             except Exception as e:
                 self.fail(str(e))
-
 
     def test_tabular_data_processor_output(self):
         '''
         Validate that the outputs from prior tests match test data.
         '''
 
-        proc_data_dir: list = os.listdir(self.data_path)
+        # Iterate through testing and processed data to validate they match.
+        proc_data_dir: list = [file for file in os.listdir(self.data_path) if os.path.isdir(file)]
         test_data_dir: list = os.listdir(self.data_output_path)
         for proc_data_path, test_data_path in zip(proc_data_dir, test_data_dir):
-            proc_data: pd.DataFrame = pd.read_csv(os.path.join(self.data_path, proc_data_path), sep='\t')
-            test_data: pd.DataFrame = pd.read_csv(os.path.join(self.data_output_path, test_data_path), sep='\t')
+            try:
+                proc_data: pd.DataFrame = pd.read_csv(os.path.join(self.data_path, proc_data_path), sep='\t')
+                test_data: pd.DataFrame = pd.read_csv(os.path.join(self.data_output_path, test_data_path), sep='\t')
+
+            except Exception as e:
+                print(os.path.join(self.data_path, proc_data_path))
+                print(os.path.join(self.data_output_path, test_data_path))
+                raise e
 
             # Validate that all data is correct.
             assert_frame_equal(proc_data, test_data)
 
+        # Clean up testing directory.
+        shutil.rmtree(self.data_output_path)
